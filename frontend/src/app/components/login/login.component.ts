ec215d619b2e4018a27e821ebe88af3180326bb1
Vangelis <vtsiat1@gmail.com>
2018-07-02 15:12:15 +0300

    Compilation with Maven fixed.
    Run './mvnw clean install' from project's root directory to export 'linkdin-frontend-app.jar' and 'linkdin-backend-app.jar'

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute, Router} from "@angular/router";
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
 
   constructor(
