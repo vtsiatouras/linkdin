629c3aceda51c6c5a3c12a3a6c8c3a2a99e6a934
Vangelis <vtsiat1@gmail.com>
2018-07-05 01:16:12 +0300

    Angular login component prints credentials

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,12 +1,16 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
 
 import {AppComponent} from './app.component';
 import {LoginComponent} from './components/login/login.component';
 import {FooterComponent} from './components/footer/footer.component';
 import {HeaderComponent} from './components/header/header.component';
 import {RegisterComponent} from './components/register/register.component';
+
+
 
 const appRoutes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
@@ -23,7 +27,9 @@ const appRoutes: Routes = [
     RegisterComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
+    FormsModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
