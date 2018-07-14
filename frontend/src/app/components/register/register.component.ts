cac48a3d92689d1afb0c24049d335396225828b3
Vangelis <vtsiat1@gmail.com>
2018-07-14 21:06:07 +0300

    Added typical form validation at login/reg forms.
    Create cookie ONLY on successful login fixed!

--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import {ActivatedRoute, Router} from '@angular/router';
+import {AbstractControl} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
