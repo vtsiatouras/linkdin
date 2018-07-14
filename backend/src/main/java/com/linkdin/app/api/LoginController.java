2c2b0062743e18a61ebef555f15b616b53f02493
Vangelis Tsiatouras <vangelis.tsiatouras@gmail.com>
2018-07-14 21:06:07 +0300

    Added typical form validation at login/reg forms.
    Create cookie ONLY on successful login fixed!

--- backend/src/main/java/com/linkdin/app/api/LoginController.java
+++ backend/src/main/java/com/linkdin/app/api/LoginController.java
@@ -9,6 +9,8 @@ import org.springframework.session.FindByIndexNameSessionRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 @RestController
@@ -18,11 +20,16 @@ public class LoginController {
     UserService userService;
 
     @PostMapping(path = "/login")
-    public ResponseEntity<String> login(@RequestBody Credentials credentials, HttpSession session) {
+    public ResponseEntity<String> login(@RequestBody Credentials credentials, HttpServletRequest request)  {
         System.out.println(credentials.email + " " + credentials.password);
+        // TODO HANDLE NULL CREDENTIALS!!! (kai sto register)
         // Check if credentials are legit
         if (userService.authenticate(credentials.email, credentials.password)) {
             String email = credentials.email;
+
+            // Todo generate user token
+            // Create a new session and add the security context.
+            HttpSession session = request.getSession(true);
             session.setAttribute(FindByIndexNameSessionRepository.PRINCIPAL_NAME_INDEX_NAME, email);
             return ResponseEntity.ok(credentials.email);
         } else {
