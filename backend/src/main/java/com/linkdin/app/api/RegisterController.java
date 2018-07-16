046898388cf69e2ffd7f9b0db86db88f148590fb
Vangelis Tsiatouras <vangelis.tsiatouras@gmail.com>
2018-07-16 22:24:48 +0300

    Register now can send image and reg controller can read it

--- backend/src/main/java/com/linkdin/app/api/RegisterController.java
+++ backend/src/main/java/com/linkdin/app/api/RegisterController.java
@@ -1,14 +1,16 @@
 package com.linkdin.app.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.linkdin.app.dto.UserRegister;
 import com.linkdin.app.model.User;
 import com.linkdin.app.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.ByteArrayOutputStream;
 
 @RestController
 public class RegisterController {
@@ -17,22 +19,42 @@ public class RegisterController {
     UserService userService;
 
     @PostMapping(path = "/register")
-    public ResponseEntity <String> register(@RequestBody UserRegister userRegister) {
+    public ResponseEntity <String> register(@RequestPart("user") String jsonUser,
+                                            @RequestPart("profileImage") MultipartFile profileImage) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            UserRegister userRegister = objectMapper.readValue(jsonUser, UserRegister.class);
+            System.err.println(userRegister.email);
 
-        // Check if email is already stored
-        if (userService.emailExist(userRegister.email)) {
-            System.out.println("EMAIL EXISTS");
+            // Check if email is already stored
+            if (userService.emailExist(userRegister.email)) {
+                System.out.println("EMAIL EXISTS");
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+
+            // Check if the passwords are not matching
+            if (!userRegister.passwordCheck(userRegister)) {
+                System.out.println("PASSWORDS DO NOT MATCH");
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+
+            // TODO STORE THE IMAGE
+            try {
+                String content = new String(profileImage.getBytes());
+                System.err.println("IMAGE\n" + content);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
+            // Store new user
+            User user = userRegister.transformToUser(userRegister);
+            userService.storeUser(user);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception ex) {
+            ex.printStackTrace();
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
-
-        // Check if the passwords are not matching
-        if(!userRegister.passwordCheck(userRegister)){
-            System.out.println("PASSWORDS DO NOT MATCH");
-            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-        }
-        // Store new user
-        User user = userRegister.transformToUser(userRegister);
-        userService.storeUser(user);
-        return new ResponseEntity<>(HttpStatus.OK);
     }
+
+
 }
--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -15,6 +15,8 @@ export class RegisterComponent implements OnInit {
   firstName: string;
   lastName: string;
   phoneNumber: string;
+  fileToUpload: File = null;
+
 
   constructor(
     private route: ActivatedRoute,
@@ -26,19 +28,39 @@ export class RegisterComponent implements OnInit {
   }
 
   registerUser() {
-    const req = this.http.post('http://localhost:8080/api/register', {
-      email: this.email,
-      password_1: this.password_1,
-      password_2: this.password_2,
-      firstName: this.firstName,
-      lastName: this.lastName,
-      phoneNumber: this.phoneNumber
-    }, {responseType: 'text', withCredentials: true}).subscribe((data: any) => {
+
+    const formData:FormData = new FormData();
+
+    const user = {
+      "email": this.email,
+      "password_1": this.password_1,
+      "password_2": this.password_2,
+      "firstName": this.firstName,
+      "lastName": this.lastName,
+      "phoneNumber": this.phoneNumber};
+
+    const image = {
+        "imageFile": this.fileToUpload
+      }
+
+    formData.append('user', JSON.stringify(user));
+    formData.append('profileImage', this.fileToUpload);
+
+    console.log(formData);
+
+    const req = this.http.post('http://localhost:8080/api/register',
+      formData
+     , {responseType: 'text', withCredentials: true}).subscribe((data: any) => {
       this.router.navigate(['/login']); // TODO make a pop up that says "Register completed"
       },
       (err: HttpErrorResponse) => {
         console.log(err); // TODO this will be trigger if the server is down or the email already exists.
     });
   }
+
+  handleFileInput(files: FileList) {
+    this.fileToUpload = files.item(0);
+  }
+
 
 }
