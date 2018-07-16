5a0f1f760741fbdcdca2cf33d206ead1405d8f26
Vangelis Tsiatouras <vangelis.tsiatouras@gmail.com>
2018-07-16 20:24:39 +0300

    Added navigation bar with dropdown menu. Fixed minor prob wtih lastname

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  firstName = localStorage.getItem('firstName');
+  lastName = localStorage.getItem('lastName');
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private http: HttpClient
+  ) { }
 
   ngOnInit() {
+  }
+
+  logoutUser() {
+    console.log('logout');
+    localStorage.clear();
+    const req = this.http.post('http://localhost:8080/api/logout', {},
+      {responseType: 'text', withCredentials: true}).subscribe((data: any) => {
+        this.router.navigate(['/login']);
+      },
+      (err: HttpErrorResponse) => {
+        console.log(err);
+      });
   }
 
 }
