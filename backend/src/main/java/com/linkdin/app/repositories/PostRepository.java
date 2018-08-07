61b510660a767533fb0d2515e90a484eebc8c9ac
Vangelis <vtsiat1@gmail.com>
2018-08-08 01:26:32 +0300

    Show most recent posts first

--- backend/src/main/java/com/linkdin/app/repositories/PostRepository.java
+++ backend/src/main/java/com/linkdin/app/repositories/PostRepository.java
@@ -10,5 +10,5 @@ import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
-    Page<Post> findByuserId(Pageable pageable, Integer userID);
+    Page<Post> findByuserIdOrderByTimestampDesc(Pageable pageable, Integer userID);
 }
