dfe5fcfc8976b3e16235373c49e32a8811241fd4
Vangelis Tsiatouras <vangelis.tsiatouras@gmail.com>
2018-08-27 22:05:49 +0300

    Added fade in out effect at post comment button

--- frontend/src/app/components/post/post.component.ts
+++ frontend/src/app/components/post/post.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { trigger, transition, animate, style } from '@angular/animations';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { ActivatedRoute, Router } from '@angular/router';
 import { environment } from '../../../environments/environment';
@@ -7,6 +8,17 @@ import { environment } from '../../../environments/environment';
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css'],
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [   // :enter is alias to 'void => *'
+        style({ opacity: 0 }),
+        animate(500, style({ opacity: 1 }))
+      ]),
+      transition(':leave', [   // :leave is alias to '* => void'
+        animate(500, style({ opacity: 0 }))
+      ])
+    ])
+  ]
 })
 export class PostComponent implements OnInit {
 
@@ -138,29 +150,33 @@ export class PostComponent implements OnInit {
       });
   }
 
-  loadComments() {
+  viewComments() {
     if (this.comments.length > 0) {
       this.showComments = !this.showComments;
     } else {
-      const userIdentifiers = { userToken: this.userToken, id: this.userId };
-      const commentData = { postID: this.postId };
-      const API_URL = environment.API_URL;
-      const req = this.http.post(API_URL + '/api/getcomments', {
-        userIdentifiers,
-        commentData
-      }, { responseType: 'text', withCredentials: true }).subscribe((data: any) => {
-        const obj = JSON.parse(data);
-        this.comments = obj;
-        for (let i = 0; i < this.comments.length; i++) {
-          this.comments[i].image = 'data:image/jpeg;base64,' + this.comments[i].image;
-        }
-        this.showComments = true;
-        console.log(obj);
-      },
-        (err: HttpErrorResponse) => {
-          console.log(err);
-        });
+      this.loadComments();
     }
+  }
+
+  loadComments() {
+    const userIdentifiers = { userToken: this.userToken, id: this.userId };
+    const commentData = { postID: this.postId };
+    const API_URL = environment.API_URL;
+    const req = this.http.post(API_URL + '/api/getcomments', {
+      userIdentifiers,
+      commentData
+    }, { responseType: 'text', withCredentials: true }).subscribe((data: any) => {
+      const obj = JSON.parse(data);
+      this.comments = obj;
+      for (let i = 0; i < this.comments.length; i++) {
+        this.comments[i].image = 'data:image/jpeg;base64,' + this.comments[i].image;
+      }
+      this.showComments = true;
+      console.log(obj);
+    },
+      (err: HttpErrorResponse) => {
+        console.log(err);
+      });
   }
 
   postComment() {
