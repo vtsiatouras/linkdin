0a32acd0783624db1268ef87f49fc470824c6af5
Nikos Sofras <sofrasnikos@hotmail.com>
2018-09-22 02:47:45 +0300

    Added TLS
    You need to edit /etc/hosts file. Add the following line: 127.0.0.1 linkdin.dyndns.org. From now on we need to put https on the url on the browser.

--- backend/src/main/java/com/linkdin/app/Application.java
+++ backend/src/main/java/com/linkdin/app/Application.java
@@ -41,7 +41,7 @@ public class Application {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedMethods("*").allowedOrigins("http://localhost:4200");
+                registry.addMapping("/**").allowedMethods("*").allowedOrigins("https://linkdin.dyndns.org:4200");
             }
         };
     }
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: 'http://localhost:8080'
+  API_URL: 'https://linkdin.dyndns.org:8080'
 };
