ff7d373c2868dc8b262a26f7bfe59cd0ba6639f1
Vangelis Tsiatouras <vangelis.tsiatouras@gmail.com>
2020-09-08 02:26:36 +0300

    [refactor]: 'connectstatus/' endpoint is now transformed to GET action

--- frontend/src/app/components/userprofile/userprofile.component.ts
+++ frontend/src/app/components/userprofile/userprofile.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { ActivatedRoute, Router } from '@angular/router';
-import { environment } from '../../../environments/environment';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {ActivatedRoute, Router} from '@angular/router';
+import {environment} from '../../../environments/environment';
 
 @Component({
   selector: 'app-userprofile',
@@ -47,7 +47,8 @@ export class UserprofileComponent implements OnInit {
     private route: ActivatedRoute,
     private router: Router,
     private http: HttpClient
-  ) { }
+  ) {
+  }
 
   ngOnInit() {
 
@@ -67,21 +68,21 @@ export class UserprofileComponent implements OnInit {
   }
 
   checkIfAdmin() {
-    const userIdentifiers = { userToken: this.userToken, id: this.userId };
+    const userIdentifiers = {userToken: this.userToken, id: this.userId};
     const API_URL = environment.API_URL;
     const req = this.http.post(API_URL + '/api/isadmin', {
       userIdentifiers
-    }, { responseType: 'text', withCredentials: true }).subscribe((data: any) => {
-      const obj = JSON.parse(data);
-      if (obj === true) {
-        this.hideElements = true;
-      } else {
-        this.hideElements = false;
-      }
-    },
+    }, {responseType: 'text', withCredentials: true}).subscribe((data: any) => {
+        const obj = JSON.parse(data);
+        if (obj === true) {
+          this.hideElements = true;
+        } else {
+          this.hideElements = false;
+        }
+      },
       (err: HttpErrorResponse) => {
         console.log(err);
-        this.router.navigate(['/error', false], { skipLocationChange: true });
+        this.router.navigate(['/error', false], {skipLocationChange: true});
       });
   }
 
@@ -94,129 +95,129 @@ export class UserprofileComponent implements OnInit {
     } else {
       this.checkConnectStatus();
     }
-    const userIdentifiers = { userToken: this.userToken, id: this.userId };
-    const requestProfile = { profileUserID: this.profileUserID };
+    const userIdentifiers = {userToken: this.userToken, id: this.userId};
+    const requestProfile = {profileUserID: this.profileUserID};
     const API_URL = environment.API_URL;
     const req = this.http.post(API_URL + '/api/user', {
       userIdentifiers,
       requestProfile
-    }, { responseType: 'text', withCredentials: true }).subscribe((data: any) => {
-      const obj = JSON.parse(data);
-      this.profileImage = 'data:image/jpeg;base64,' + obj.profileImage;
-      const userObj = JSON.parse(obj.user);
-      this.profileFirstName = userObj.firstName;
-      this.profileSurname = userObj.lastName;
-      this.getUserInfo();
-      this.getPosts();
-    },
+    }, {responseType: 'text', withCredentials: true}).subscribe((data: any) => {
+        const obj = JSON.parse(data);
+        this.profileImage = 'data:image/jpeg;base64,' + obj.profileImage;
+        const userObj = JSON.parse(obj.user);
+        this.profileFirstName = userObj.firstName;
+        this.profileSurname = userObj.lastName;
+        this.getUserInfo();
+        this.getPosts();
+      },
       (err: HttpErrorResponse) => {
         console.log(err);
-        this.router.navigate(['/error', false], { skipLocationChange: true });
+        this.router.navigate(['/error', false], {skipLocationChange: true});
       });
   }
 
   getUserInfo() {
-    const userIdentifiers = { userToken: this.userToken, id: this.userId };
-    const userInfoRequest = { userIdInfo: this.profileUserID.toString() };
+    const userIdentifiers = {userToken: this.userToken, id: this.userId};
+    const userInfoRequest = {userIdInfo: this.profileUserID.toString()};
     const API_URL = environment.API_URL;
     const req = this.http.post(API_URL + '/api/getuserinfo', {
       userIdentifiers,
       userInfoRequest
-    }, { responseType: 'text', withCredentials: true }).subscribe((data: any) => {
-      const obj = JSON.parse(data);
-      this.profilePhoneNumber = obj.phoneNumber;
-      this.profileCity = obj.city;
-      this.profileProfession = obj.profession;
-      this.profileCompany = obj.company;
-      this.profileEducation = obj.education;
-    },
+    }, {responseType: 'text', withCredentials: true}).subscribe((data: any) => {
+        const obj = JSON.parse(data);
+        this.profilePhoneNumber = obj.phoneNumber;
+        this.profileCity = obj.city;
+        this.profileProfession = obj.profession;
+        this.profileCompany = obj.company;
+        this.profileEducation = obj.education;
+      },
       (err: HttpErrorResponse) => {
         console.log(err);
-        this.router.navigate(['/error', false], { skipLocationChange: true });
+        this.router.navigate(['/error', false], {skipLocationChange: true});
       });
   }
 
   getPosts() {
-    const userIdentifiers = { userToken: this.userToken, id: this.userId };
-    const pageRequest = { profileUserID: this.profileUserID, pageNumber: this.page, limit: this.limitPosts };
+    const userIdentifiers = {userToken: this.userToken, id: this.userId};
+    const pageRequest = {profileUserID: this.profileUserID, pageNumber: this.page, limit: this.limitPosts};
     this.page++;
     const API_URL = environment.API_URL;
     const req = this.http.post(API_URL + '/api/getprofileposts', {
       userIdentifiers,
       pageRequest
-    }, { responseType: 'text', withCredentials: true }).subscribe((data: any) => {
-      const obj = JSON.parse(data);
-      this.totalPosts = obj.totalElements;
-      if (this.totalPosts > 0) {
-        const numberOfPosts = obj.numberOfElements;
-        this.showedPosts = this.showedPosts + numberOfPosts;
-        if (this.totalPosts > this.showedPosts) {
-          this.loadMoreButton = true;
+    }, {responseType: 'text', withCredentials: true}).subscribe((data: any) => {
+        const obj = JSON.parse(data);
+        this.totalPosts = obj.totalElements;
+        if (this.totalPosts > 0) {
+          const numberOfPosts = obj.numberOfElements;
+          this.showedPosts = this.showedPosts + numberOfPosts;
+          if (this.totalPosts > this.showedPosts) {
+            this.loadMoreButton = true;
+          } else {
+            this.loadMoreButton = false;
+          }
+          for (let i = 0; i < numberOfPosts; i++) {
+            this.posts.push(obj.content[i]);
+          }
         } else {
-          this.loadMoreButton = false;
+          this.posts = null;
         }
-        for (let i = 0; i < numberOfPosts; i++) {
-          this.posts.push(obj.content[i]);
-        }
-      } else {
-        this.posts = null;
-      }
-    },
+      },
       (err: HttpErrorResponse) => {
         console.log(err);
-        this.router.navigate(['/error', false], { skipLocationChange: true });
+        this.router.navigate(['/error', false], {skipLocationChange: true});
       });
   }
 
   checkConnectStatus() {
-    const userIdentifiers = { userToken: this.userToken, id: this.userId };
-    const targetProfile = { profileUserID: this.profileUserID.toString() };
+    const userIdentifiers = {userToken: this.userToken, id: this.userId};
+    const targetProfile = {profileUserID: this.profileUserID.toString()};
+    const api_params = {...userIdentifiers, ...targetProfile}
     const API_URL = environment.API_URL;
-    const req = this.http.post(API_URL + '/api/connectstatus', {
-      userIdentifiers,
-      targetProfile
-    }, { responseType: 'text', withCredentials: true }).subscribe((data: any) => {
-      const obj = JSON.parse(data);
-      if (obj.friends === '0') {
-        this.requestConnectButton = true;
-        this.connectPendingButton = false;
-        this.connectedButton = false;
-        this.networkButton = false;
-      } else {
-        // Show network if only you are connected to this person
-        if (obj.pending === '1') {
-          this.networkButton = false;
-          this.requestConnectButton = false;
-          this.connectPendingButton = true;
-          this.connectedButton = false;
-        } else {
-          // They are connected
-          this.networkButton = true;
-          this.requestConnectButton = false;
+    const req = this.http.get(API_URL + '/api/connectstatus', {
+      params: api_params, responseType: 'text', withCredentials: true
+    }).subscribe((data: any) => {
+        const obj = JSON.parse(data);
+        if (obj.friends === '0') {
+          this.requestConnectButton = true;
           this.connectPendingButton = false;
-          this.connectedButton = true;
+          this.connectedButton = false;
+          this.networkButton = false;
+        } else {
+          // Show network if only you are connected to this person
+          if (obj.pending === '1') {
+            this.networkButton = false;
+            this.requestConnectButton = false;
+            this.connectPendingButton = true;
+            this.connectedButton = false;
+          } else {
+            // They are connected
+            this.networkButton = true;
+            this.requestConnectButton = false;
+            this.connectPendingButton = false;
+            this.connectedButton = true;
+          }
         }
-      }
-    },
+      },
       (err: HttpErrorResponse) => {
         console.log(err);
-        this.router.navigate(['/error', false], { skipLocationChange: true });
+        this.router.navigate(['/error', false], {skipLocationChange: true});
       });
   }
 
   sendConnectRequest() {
-    const userIdentifiers = { userToken: this.userToken, id: this.userId };
-    const friendRequest = { userRequestID: this.profileUserID.toString() };
+    const userIdentifiers = {userToken: this.userToken, id: this.userId};
+    const friendRequest = {userRequestID: this.profileUserID.toString()};
     const API_URL = environment.API_URL;
     const req = this.http.post(API_URL + '/api/sendconnect', {
       userIdentifiers,
       friendRequest
-    }, { responseType: 'text', withCredentials: true }).subscribe((data: any) => {
-      this.checkConnectStatus();
-    },
+    }, {responseType: 'text', withCredentials: true}).subscribe((data: any) => {
+        this.checkConnectStatus();
+      },
       (err: HttpErrorResponse) => {
         console.log(err);
-        this.router.navigate(['/error', false], { skipLocationChange: true });
+        this.router.navigate(['/error', false], {skipLocationChange: true});
       });
   }
 
