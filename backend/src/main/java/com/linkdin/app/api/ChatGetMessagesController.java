d83311edc6271c006d43ceb34bedb71d9c18294a
Vangelis Tsiatouras <vangelis.tsiatouras@gmail.com>
2020-09-08 02:26:56 +0300

    [chore]: removed unused imports

--- backend/src/main/java/com/linkdin/app/api/ChatGetMessagesController.java
+++ backend/src/main/java/com/linkdin/app/api/ChatGetMessagesController.java
@@ -1,15 +1,12 @@
 package com.linkdin.app.api;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.linkdin.app.dto.UserIdentifiers;
 import com.linkdin.app.services.AuthRequestService;
 import com.linkdin.app.services.ChatService;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
