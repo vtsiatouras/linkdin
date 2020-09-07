7645ba597d240283d7202b15a69ddf112dc8c2dd
Vangelis Tsiatouras <vangelis.tsiatouras@gmail.com>
2020-09-07 03:37:56 +0300

    [refactor]: 'getactivechats/' endpoint now is transformed to GET, front-end is patched

--- backend/src/main/java/com/linkdin/app/api/ChatGetActiveChatsController.java
+++ backend/src/main/java/com/linkdin/app/api/ChatGetActiveChatsController.java
@@ -1,16 +1,12 @@
 package com.linkdin.app.api;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.linkdin.app.dto.UserIdentifiers;
 import com.linkdin.app.services.AuthRequestService;
 import com.linkdin.app.services.ChatService;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
@@ -22,14 +18,9 @@ public class ChatGetActiveChatsController {
     @Autowired
     AuthRequestService authRequestService;
 
-    @PostMapping(path = "/getactivechats")
-    public ResponseEntity<Object> getActiveChats(@RequestBody String jsonChatRequest, HttpSession session) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        JSONObject obj = new JSONObject(jsonChatRequest);
+    @GetMapping(path = "/getactivechats")
+    public ResponseEntity<Object> getActiveChats(UserIdentifiers userIdentifiers, HttpSession session) {
         try {
-            JSONObject userObj = obj.getJSONObject("userIdentifiers");
-            UserIdentifiers userIdentifiers = objectMapper.readValue(userObj.toString(), UserIdentifiers.class);
-
             // Authenticate user
             if (!authRequestService.authenticateRequest(userIdentifiers, session)) {
                 return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
