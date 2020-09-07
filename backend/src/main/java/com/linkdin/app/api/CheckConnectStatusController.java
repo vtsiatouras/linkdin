13ea6e860f36607a32e15f75b42eb625e68b5eac
Vangelis Tsiatouras <vangelis.tsiatouras@gmail.com>
2020-09-08 02:26:56 +0300

    [chore]: removed unused imports

--- backend/src/main/java/com/linkdin/app/api/CheckConnectStatusController.java
+++ backend/src/main/java/com/linkdin/app/api/CheckConnectStatusController.java
@@ -8,13 +8,10 @@ import com.linkdin.app.model.UserNetwork;
 import com.linkdin.app.services.AuthRequestService;
 import com.linkdin.app.services.UserNetworkService;
 import com.linkdin.app.services.UserService;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
