9466ea9c8c6c22807478af8cbb7f840434beeffb
Vangelis Tsiatouras <vangelis.tsiatouras@gmail.com>
2020-09-08 02:01:24 +0300

    [refactor]: 'getchatmessages/' endpoint now is transformed to GET action

--- backend/src/main/java/com/linkdin/app/api/ChatGetMessagesController.java
+++ backend/src/main/java/com/linkdin/app/api/ChatGetMessagesController.java
@@ -8,11 +8,10 @@ import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.persistence.criteria.CriteriaBuilder;
 import javax.servlet.http.HttpSession;
 import java.util.List;
 
@@ -23,22 +22,16 @@ public class ChatGetMessagesController {
     @Autowired
     AuthRequestService authRequestService;
 
-    @PostMapping(path = "/getchatmessages")
-    public ResponseEntity<Object> getChatMessages(@RequestBody String jsonChatRequest, HttpSession session) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        JSONObject obj = new JSONObject(jsonChatRequest);
-        try {
-            JSONObject userObj = obj.getJSONObject("userIdentifiers");
-            JSONObject chatMessageObj = obj.getJSONObject("chatMessageContent");
-            UserIdentifiers userIdentifiers = objectMapper.readValue(userObj.toString(), UserIdentifiers.class);
-            Integer chatID = chatMessageObj.getInt("chatID");
+    @GetMapping(path = "/getchatmessages")
+    public ResponseEntity<Object> getChatMessages(UserIdentifiers userIdentifiers, Integer chatID, HttpSession session) {
 
+        try {
             // Authenticate user
             if (!authRequestService.authenticateRequest(userIdentifiers, session)) {
                 return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
             }
 
-            if(!chatService.checkChatWithUserID(chatID, Integer.parseInt(userIdentifiers.id))) {
+            if (!chatService.checkChatWithUserID(chatID, Integer.parseInt(userIdentifiers.id))) {
                 return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
             }
 
