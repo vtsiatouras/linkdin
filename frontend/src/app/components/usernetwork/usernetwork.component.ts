3bff3eb42a45ef1c020b3ac5231d3f7f41699807
Vangelis Tsiatouras <vangelis.tsiatouras@gmail.com>
2020-09-14 02:39:29 +0300

    [refactor]: '/getconnectedusers' endpoint now is transformed to GET action

--- frontend/src/app/components/usernetwork/usernetwork.component.ts
+++ frontend/src/app/components/usernetwork/usernetwork.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { ActivatedRoute, Router } from '@angular/router';
-import { environment } from '../../../environments/environment';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {ActivatedRoute, Router} from '@angular/router';
+import {environment} from '../../../environments/environment';
 
 @Component({
   selector: 'app-usernetwork',
@@ -21,7 +21,8 @@ export class UsernetworkComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private http: HttpClient) { }
+    private http: HttpClient) {
+  }
 
   ngOnInit() {
     this.route.params.subscribe((params) => {
@@ -34,23 +35,25 @@ export class UsernetworkComponent implements OnInit {
   }
 
   getConnectedUsers() {
-    const userIdentifiers = { userToken: this.userToken, id: this.userId };
-    const profileNetwork = { profileUserID: this.profileUserID.toString() };
+    const userIdentifiers = {userToken: this.userToken, id: this.userId};
+    const profile = {profileUserID: this.profileUserID.toString()};
+    const params = {...userIdentifiers, ...profile}
     const API_URL = environment.API_URL;
-    const req = this.http.post(API_URL + '/api/getconnectedusers', {
-      userIdentifiers,
-      profileNetwork
-    }, { responseType: 'text', withCredentials: true }).subscribe((data: any) => {
-      const obj = JSON.parse(data);
-      this.totalUsers = obj.numberOfResults;
-      for (let i = 0; i < this.totalUsers; i++) {
-        this.users.push(obj.list[i]);
-        this.users[i].image = 'data:image/jpeg;base64,' + this.users[i].image;
-      }
-    },
+    const req = this.http.get(API_URL + '/api/getconnectedusers', {
+      params: params,
+      responseType: 'text',
+      withCredentials: true
+    }).subscribe((data: any) => {
+        const obj = JSON.parse(data);
+        this.totalUsers = obj.numberOfResults;
+        for (let i = 0; i < this.totalUsers; i++) {
+          this.users.push(obj.list[i]);
+          this.users[i].image = 'data:image/jpeg;base64,' + this.users[i].image;
+        }
+      },
       (err: HttpErrorResponse) => {
         console.log(err);
-        this.router.navigate(['/error', false], { skipLocationChange: true });
+        this.router.navigate(['/error', false], {skipLocationChange: true});
       });
   }
 
