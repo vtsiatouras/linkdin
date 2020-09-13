92c57a4034270854052ff090cd9948f20905fc11
Vangelis Tsiatouras <vangelis.tsiatouras@gmail.com>
2020-09-14 02:55:02 +0300

    [refactor]: '/getconnectrequests' endpoint now is transformed to GET action

--- backend/src/main/java/com/linkdin/app/api/GetConnectRequestsController.java
+++ backend/src/main/java/com/linkdin/app/api/GetConnectRequestsController.java
@@ -1,16 +1,14 @@
 package com.linkdin.app.api;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.linkdin.app.dto.ListUsers;
 import com.linkdin.app.dto.UserIdentifiers;
 import com.linkdin.app.services.AuthRequestService;
 import com.linkdin.app.services.UserNetworkService;
-import org.json.JSONObject;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
@@ -23,14 +21,9 @@ public class GetConnectRequestsController {
     @Autowired
     AuthRequestService authRequestService;
 
-    @PostMapping(path = "/getconnectrequests")
-    public ResponseEntity<Object> connectRequests(@RequestBody String jsonGetRequests, HttpSession session) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        JSONObject obj = new JSONObject(jsonGetRequests);
+    @GetMapping(path = "/getconnectrequests")
+    public ResponseEntity<Object> connectRequests(UserIdentifiers userIdentifiers, HttpSession session) {
         try {
-            JSONObject userObj = obj.getJSONObject("userIdentifiers");
-            UserIdentifiers userIdentifiers = objectMapper.readValue(userObj.toString(), UserIdentifiers.class);
-
             // Authenticate user
             if (!authRequestService.authenticateRequest(userIdentifiers, session)) {
                 return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
--- frontend/src/app/components/notifications/notifications.component.ts
+++ frontend/src/app/components/notifications/notifications.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { ActivatedRoute, Router } from '@angular/router';
-import { environment } from '../../../environments/environment';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {ActivatedRoute, Router} from '@angular/router';
+import {environment} from '../../../environments/environment';
 
 @Component({
   selector: 'app-notifications',
@@ -25,9 +25,11 @@ export class NotificationsComponent implements OnInit {
     private route: ActivatedRoute,
     private router: Router,
     private http: HttpClient
-  ) { }
+  ) {
+  }
 
-  ngOnInit() { }
+  ngOnInit() {
+  }
 
   renderNotifications() {
     this.getNotifications();
@@ -38,71 +40,71 @@ export class NotificationsComponent implements OnInit {
   getNotifications() {
     this.pendingRequests = [];
     this.totalPendingRequests = 0;
-    const userIdentifiers = { userToken: this.userToken, id: this.userId };
+    const userIdentifiers = {userToken: this.userToken, id: this.userId};
     const API_URL = environment.API_URL;
     const req = this.http.post(API_URL + '/api/getnotifications', {
       userIdentifiers,
-    }, { responseType: 'text', withCredentials: true }).subscribe((data: any) => {
-      const obj = JSON.parse(data);
-      this.notifications = obj;
-      this.totalNotifications = this.notifications.length;
-    },
+    }, {responseType: 'text', withCredentials: true}).subscribe((data: any) => {
+        const obj = JSON.parse(data);
+        this.notifications = obj;
+        this.totalNotifications = this.notifications.length;
+      },
       (err: HttpErrorResponse) => {
         console.log(err);
-        this.router.navigate(['/error', false], { skipLocationChange: true });
+        this.router.navigate(['/error', false], {skipLocationChange: true});
       });
   }
 
   getPendingConnectRequests() {
     this.pendingRequests = [];
     this.totalPendingRequests = 0;
-    const userIdentifiers = { userToken: this.userToken, id: this.userId };
+    const userIdentifiers = {userToken: this.userToken, id: this.userId};
     const API_URL = environment.API_URL;
-    const req = this.http.post(API_URL + '/api/getconnectrequests', {
-      userIdentifiers,
-    }, { responseType: 'text', withCredentials: true }).subscribe((data: any) => {
-      const obj = JSON.parse(data);
-      this.totalPendingRequests = obj.numberOfResults;
-      if (this.totalPendingRequests > 0) {
-        for (let i = 0; i < this.totalPendingRequests; i++) {
-          this.pendingRequests.push(obj.list[i]);
-          this.pendingRequests[i].image = 'data:image/jpeg;base64,' + this.pendingRequests[i].image;
+    const req = this.http.get(API_URL + '/api/getconnectrequests', {
+      params: userIdentifiers, responseType: 'text', withCredentials: true
+    }).subscribe((data: any) => {
+        const obj = JSON.parse(data);
+        this.totalPendingRequests = obj.numberOfResults;
+        if (this.totalPendingRequests > 0) {
+          for (let i = 0; i < this.totalPendingRequests; i++) {
+            this.pendingRequests.push(obj.list[i]);
+            this.pendingRequests[i].image = 'data:image/jpeg;base64,' + this.pendingRequests[i].image;
+          }
         }
-      }
-    },
+      },
       (err: HttpErrorResponse) => {
         console.log(err);
-        this.router.navigate(['/error', false], { skipLocationChange: true });
+        this.router.navigate(['/error', false], {skipLocationChange: true});
       });
   }
 
   acceptConnectRequest(index) {
-    const userIdentifiers = { userToken: this.userToken, id: this.userId };
-    const connectRequest = { profileUserID: this.pendingRequests[index].id, accepted: '1' };
+    const userIdentifiers = {userToken: this.userToken, id: this.userId};
+    const connectRequest = {profileUserID: this.pendingRequests[index].id, accepted: '1'};
     const API_URL = environment.API_URL;
     const req = this.http.post(API_URL + '/api/handleconnectrequest', {
       userIdentifiers,
       connectRequest
-    }, { responseType: 'text', withCredentials: true }).subscribe((data: any) => {
-    },
+    }, {responseType: 'text', withCredentials: true}).subscribe((data: any) => {
+      },
       (err: HttpErrorResponse) => {
         console.log(err);
-        this.router.navigate(['/error', false], { skipLocationChange: true });
+        this.router.navigate(['/error', false], {skipLocationChange: true});
       });
   }
 
   declineConnectRequest(index) {
-    const userIdentifiers = { userToken: this.userToken, id: this.userId };
-    const connectRequest = { profileUserID: this.pendingRequests[index].id, accepted: '0' };
+    const userIdentifiers = {userToken: this.userToken, id: this.userId};
+    const connectRequest = {profileUserID: this.pendingRequests[index].id, accepted: '0'};
     const API_URL = environment.API_URL;
     const req = this.http.post(API_URL + '/api/handleconnectrequest', {
       userIdentifiers,
       connectRequest
-    }, { responseType: 'text', withCredentials: true }).subscribe((data: any) => {
-    },
+    }, {responseType: 'text', withCredentials: true}).subscribe((data: any) => {
+      },
       (err: HttpErrorResponse) => {
         console.log(err);
-        this.router.navigate(['/error', false], { skipLocationChange: true });
+        this.router.navigate(['/error', false], {skipLocationChange: true});
       });
   }
 
