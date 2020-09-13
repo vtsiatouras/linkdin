17cb874989130ba8ce2befdd95c7ec1698a5b5e1
Vangelis Tsiatouras <vangelis.tsiatouras@gmail.com>
2020-09-14 01:42:09 +0300

    [refactor]: '/getchatbyid' endpoint now is transformed to GET action

--- backend/src/main/java/com/linkdin/app/api/GetChatByIdController.java
+++ backend/src/main/java/com/linkdin/app/api/GetChatByIdController.java
@@ -1,16 +1,14 @@
 package com.linkdin.app.api;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.linkdin.app.dto.UserIdentifiers;
 import com.linkdin.app.model.Chat;
 import com.linkdin.app.services.AuthRequestService;
 import com.linkdin.app.services.ChatService;
-import org.json.JSONObject;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
@@ -22,16 +20,9 @@ public class GetChatByIdController {
     @Autowired
     AuthRequestService authRequestService;
 
-    @PostMapping(path = "/getchatbyid")
-    public ResponseEntity<Object> getChatID(@RequestBody String jsonChatRequest, HttpSession session) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        JSONObject obj = new JSONObject(jsonChatRequest);
+    @GetMapping(path = "/getchatbyid")
+    public ResponseEntity<Object> getChatID(UserIdentifiers userIdentifiers, String chatID, HttpSession session) {
         try {
-            JSONObject userObj = obj.getJSONObject("userIdentifiers");
-            JSONObject chatObj = obj.getJSONObject("chat");
-            UserIdentifiers userIdentifiers = objectMapper.readValue(userObj.toString(), UserIdentifiers.class);
-            String chatID = chatObj.getString("chatID");
-
             // Authenticate user
             if (!authRequestService.authenticateRequest(userIdentifiers, session)) {
                 return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
