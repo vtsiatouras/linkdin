aa6b0a07a6a4bf36285dada74bf41d899b3a8ba2
Vangelis Tsiatouras <vangelis.tsiatouras@gmail.com>
2020-09-14 03:22:38 +0300

    [refactor]: '/getimage' now is transformed to GET action

--- backend/src/main/java/com/linkdin/app/api/GetImageController.java
+++ backend/src/main/java/com/linkdin/app/api/GetImageController.java
@@ -10,9 +10,7 @@ import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 
@@ -24,21 +22,22 @@ public class GetImageController {
     @Autowired
     ImageStorageService imageStorageService;
 
-    @PostMapping(path = "/getimage")
-    public ResponseEntity<Object> gendImage(@RequestBody String jsonImage, HttpSession session) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        JSONObject obj = new JSONObject(jsonImage);
+    @GetMapping(path = "/getimage")
+    public ResponseEntity<Object> gendImage(UserIdentifiers userIdentifiers, @RequestParam String imageName,
+                                            HttpSession session) {
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        JSONObject obj = new JSONObject(jsonImage);
         try {
-            JSONObject userObj = obj.getJSONObject("userIdentifiers");
-            JSONObject imageObj = obj.getJSONObject("image");
-            UserIdentifiers userIdentifiers = objectMapper.readValue(userObj.toString(), UserIdentifiers.class);
-            String imageName = imageObj.getString("imageName");
+//            JSONObject userObj = obj.getJSONObject("userIdentifiers");
+//            JSONObject imageObj = obj.getJSONObject("image");
+//            UserIdentifiers userIdentifiers = objectMapper.readValue(userObj.toString(), UserIdentifiers.class);
+//            String imageName = imageObj.getString("imageName");
             // Authenticate user
             if (!authRequestService.authenticateRequest(userIdentifiers, session)) {
                 return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
             }
 
-            String imageBytes =  imageStorageService.getImage(imageName);
+            String imageBytes = imageStorageService.getImage(imageName);
             String returnJsonImage = new JSONObject()
                     .put("image", imageBytes).toString();
             return new ResponseEntity<>(returnJsonImage, HttpStatus.OK);
