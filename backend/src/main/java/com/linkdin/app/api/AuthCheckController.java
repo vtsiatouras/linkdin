9b48b91c16cd02749c621f558b046923e644afe3
Vangelis Tsiatouras <vangelis.tsiatouras@gmail.com>
2020-09-14 05:23:18 +0300

    [refactor]: move isadmin to authcontroller

--- backend/src/main/java/com/linkdin/app/api/AuthCheckController.java
+++ backend/src/main/java/com/linkdin/app/api/AuthCheckController.java
@@ -2,12 +2,14 @@ package com.linkdin.app.api;
 
 import com.linkdin.app.dto.UserIdentifiers;
 import com.linkdin.app.model.User;
+import com.linkdin.app.services.AuthRequestService;
 import com.linkdin.app.services.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.session.FindByIndexNameSessionRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +21,8 @@ import javax.servlet.http.HttpSession;
 public class AuthCheckController {
     @Autowired
     UserService userService;
+    @Autowired
+    AuthRequestService authRequestService;
 
     @PostMapping(path = "/authcheck")
     public ResponseEntity<Object> authCheck(@RequestBody UserIdentifiers userIdentifiers, HttpServletRequest request) {
@@ -100,4 +104,21 @@ public class AuthCheckController {
         }
     }
 
+    @GetMapping(path = "/isadmin")
+    public ResponseEntity<String> isAdmin(UserIdentifiers userIdentifiers, HttpSession session) {
+        try {
+            if (!authRequestService.authenticateRequest(userIdentifiers, session)) {
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+            User user = userService.returnUserByID(Integer.parseInt(userIdentifiers.id));
+            if (user.getIsAdmin() == 1) {
+                return new ResponseEntity<>("true", HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>("false", HttpStatus.OK);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
