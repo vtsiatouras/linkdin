1cda800be2d37003ce14d65c9b72029c5ad84ff0
Vangelis Tsiatouras <vangelis.tsiatouras@gmail.com>
2020-09-14 04:30:28 +0300

    [refactor]: '/searchusers' endpoint now is transformed to GET action

--- backend/src/main/java/com/linkdin/app/api/SearchController.java
+++ backend/src/main/java/com/linkdin/app/api/SearchController.java
@@ -1,17 +1,14 @@
 package com.linkdin.app.api;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.linkdin.app.dto.ListUsers;
 import com.linkdin.app.dto.UserIdentifiers;
 import com.linkdin.app.services.AuthRequestService;
 import com.linkdin.app.services.UserService;
-import org.json.JSONObject;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 
@@ -23,16 +20,10 @@ public class SearchController {
     @Autowired
     AuthRequestService authRequestService;
 
-    @PostMapping(path = "/searchusers")
-    public ResponseEntity<Object> search(@RequestBody String jsonSearchRequest, HttpSession session) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        JSONObject obj = new JSONObject(jsonSearchRequest);
+    @GetMapping(path = "/searchusers")
+    public ResponseEntity<Object> search(UserIdentifiers userIdentifiers, @RequestParam String searchQuery,
+                                         HttpSession session) {
         try {
-            JSONObject userObj = obj.getJSONObject("userIdentifiers");
-            JSONObject searchObj = obj.getJSONObject("searchData");
-            UserIdentifiers userIdentifiers = objectMapper.readValue(userObj.toString(), UserIdentifiers.class);
-            String searchQuery = searchObj.getString("searchQuery");
-
             // Authenticate user
             if (!authRequestService.authenticateRequest(userIdentifiers, session)) {
                 return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
