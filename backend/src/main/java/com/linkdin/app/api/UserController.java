53a96a24fecd5401b57c709c26dc51104f750229
Vangelis Tsiatouras <vangelis.tsiatouras@gmail.com>
2020-09-14 05:21:53 +0300

    [refactor]: merge all user actions to one file pt.2

--- backend/src/main/java/com/linkdin/app/api/UserController.java
+++ backend/src/main/java/com/linkdin/app/api/UserController.java
@@ -1,11 +1,11 @@
 package com.linkdin.app.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.linkdin.app.dto.UserBasicInfo;
 import com.linkdin.app.dto.UserIdentifiers;
+import com.linkdin.app.dto.UserInfo;
 import com.linkdin.app.model.User;
-import com.linkdin.app.services.AdminAuthRequestService;
-import com.linkdin.app.services.AuthRequestService;
-import com.linkdin.app.services.ImageStorageService;
-import com.linkdin.app.services.UserService;
+import com.linkdin.app.services.*;
 
 import org.json.JSONObject;
 
@@ -20,6 +20,8 @@ public class UserController {
 
     @Autowired
     UserService userService;
+    @Autowired
+    UserNetworkService userNetworkService;
     @Autowired
     AuthRequestService authRequestService;
     @Autowired
@@ -64,4 +66,101 @@ public class UserController {
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
     }
+
+    @GetMapping(path = "/getuserbasicinfo")
+    public ResponseEntity<Object> userBasicInfo(UserIdentifiers userIdentifiers, @RequestParam String userIdInfo,
+                                                HttpSession session) {
+        try {
+            // Authenticate user
+            if (!authRequestService.authenticateRequest(userIdentifiers, session)) {
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+
+            User user = userService.returnUserByID(Integer.parseInt(userIdInfo));
+            if (user == null) {
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+
+            UserBasicInfo userBasicInfo = new UserBasicInfo();
+            userBasicInfo.id = Integer.toString(user.getId());
+            userBasicInfo.name = user.getName();
+            userBasicInfo.surname = user.getSurname();
+            userBasicInfo.image = imageStorageService.getImage(user.getProfilePicture());
+            return new ResponseEntity<Object>(userBasicInfo, HttpStatus.OK);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping(path = "/getuserinfo")
+    public ResponseEntity<Object> userInfo(UserIdentifiers userIdentifiers, @RequestParam String userIdInfo,
+                                           HttpSession session) {
+        try {
+            // Authenticate user
+            if (!authRequestService.authenticateRequest(userIdentifiers, session)) {
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+
+            boolean isAdmin = adminAuthRequestService.authenticateRequest(userIdentifiers, session);
+
+            User user = userService.returnUserByID(Integer.parseInt(userIdInfo));
+            if (user == null) {
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+
+            // If the requested profile belongs to the user that made the request OR
+            // If the requested info belongs to a friend
+            // If admin requested
+            if (userIdInfo.equals(userIdentifiers.id) || userNetworkService
+                    .checkIfConnected(Integer.parseInt(userIdInfo), Integer.parseInt(userIdentifiers.id)) || isAdmin) {
+                UserInfo userInfo = userService.getUserInfo(userIdInfo);
+                return new ResponseEntity<Object>(userInfo, HttpStatus.OK);
+            }
+            // If not return public info
+            else {
+                UserInfo userInfo = userService.getPublicUserInfo(userIdInfo);
+                return new ResponseEntity<Object>(userInfo, HttpStatus.OK);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping(path = "/setuserinfo")
+    public ResponseEntity<Object> setUserInfo(@RequestBody String jsonRequestUserInfo, HttpSession session) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JSONObject obj = new JSONObject(jsonRequestUserInfo);
+        try {
+            JSONObject userObj = obj.getJSONObject("userIdentifiers");
+            JSONObject userInfoRequestObj = obj.getJSONObject("userInfoUpdate");
+            UserIdentifiers userIdentifiers = objectMapper.readValue(userObj.toString(), UserIdentifiers.class);
+            UserInfo userInfo = objectMapper.readValue(userInfoRequestObj.toString(), UserInfo.class);
+            ;
+
+            // Authenticate user
+            if (!authRequestService.authenticateRequest(userIdentifiers, session)) {
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+
+            User user = userService.returnUserByID(Integer.parseInt(userIdentifiers.id));
+            user.setPhoneNumber(userInfo.phoneNumber);
+            user.setPublicPhoneNumber((byte) userInfo.isPhonePublic);
+            user.setCity(userInfo.city);
+            user.setPublicCity((byte) userInfo.isCityPublic);
+            user.setProfession(userInfo.profession);
+            user.setPublicProfession((byte) userInfo.isProfessionPublic);
+            user.setCompany(userInfo.company);
+            user.setPublicCompany((byte) userInfo.isCompanyPublic);
+            user.setEducation(userInfo.education);
+            user.setPublicEducation((byte) userInfo.isEducationPublic);
+            userService.storeUser(user);
+            return new ResponseEntity<Object>(HttpStatus.OK);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+    }
+
 }
