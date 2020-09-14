bf8cd041636d762419d2dbf6a4559e2aa0c985fe
Vangelis Tsiatouras <vangelis.tsiatouras@gmail.com>
2020-09-14 05:11:00 +0300

    [refactor]: merge getfriendsactivity & getpostsfromfriends

--- backend/src/main/java/com/linkdin/app/api/FriendsActivityController.java
+++ backend/src/main/java/com/linkdin/app/api/FriendsActivityController.java
@@ -9,20 +9,48 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @RestController
-public class GetPostsFromFriendsController {
-
+public class FriendsActivityController {
     @Autowired
     PostService postService;
     @Autowired
     UserNetworkService userNetworkService;
     @Autowired
     AuthRequestService authRequestService;
+
+    @GetMapping(path = "/getfriendsactivity")
+    public ResponseEntity<Object> postsFromFriends(UserIdentifiers userIdentifiers, HttpSession session) {
+        try {
+            // Authenticate user
+            if (!authRequestService.authenticateRequest(userIdentifiers, session)) {
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+
+            List friendList = userNetworkService.getConnectedUsersIDsOnly(Integer.parseInt(userIdentifiers.id));
+
+            // Return results if only the user has friends
+            if (friendList.size() > 0) {
+                List interestsList =
+                        postService.getInterestingPostsAndFriendsIDs(friendList, Integer.parseInt(userIdentifiers.id));
+                List commentsList =
+                        postService.getCommentedPostsAndFriendsIDs(friendList, Integer.parseInt(userIdentifiers.id));
+                List result[] = {interestsList, commentsList};
+                return new ResponseEntity<Object>(result, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<Object>("null", HttpStatus.OK);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
     @GetMapping(path = "/getpostsfromfriends")
     public ResponseEntity<Object> postsFromFriends(UserIdentifiers userIdentifiers, @RequestParam String pageNumber,
