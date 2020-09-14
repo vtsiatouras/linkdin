3d55ea53389a8cdd81eebb925fc3f12f1c9c8857
Vangelis Tsiatouras <vangelis.tsiatouras@gmail.com>
2020-09-14 04:44:01 +0300

    [refactor]: merge all auth related endpoints to one file

--- backend/src/main/java/com/linkdin/app/api/AuthCheckController.java
+++ backend/src/main/java/com/linkdin/app/api/AuthCheckController.java
@@ -3,6 +3,7 @@ package com.linkdin.app.api;
 import com.linkdin.app.dto.UserIdentifiers;
 import com.linkdin.app.model.User;
 import com.linkdin.app.services.UserService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -40,6 +41,58 @@ public class AuthCheckController {
                 return new ResponseEntity<>(HttpStatus.OK);
             } else {
                 return new ResponseEntity<>("1",HttpStatus.UNAUTHORIZED);
+            }
+        } else {
+            System.err.println("user is NOT authenticated!");
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+    @PostMapping(path = "/authlightcheck")
+    public ResponseEntity<Object> authLightCheck(@RequestBody UserIdentifiers userIdentifiers, HttpServletRequest request) {
+
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+
+        if (userIdentifiers.userToken == null || userIdentifiers.id == null) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        if(session.getAttribute(FindByIndexNameSessionRepository.PRINCIPAL_NAME_INDEX_NAME) == null){
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        if (session.getAttribute(FindByIndexNameSessionRepository.PRINCIPAL_NAME_INDEX_NAME).equals(userIdentifiers.id) &&
+                session.getAttribute("userToken").equals(userIdentifiers.userToken)) {
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            System.err.println("user is NOT authenticated!");
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+    @PostMapping(path = "/admincheck")
+    public ResponseEntity<String> adminCheck(@RequestBody UserIdentifiers userIdentifiers, HttpServletRequest request) {
+
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+
+        if (userIdentifiers.userToken == null || userIdentifiers.id == null) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        if(session.getAttribute(FindByIndexNameSessionRepository.PRINCIPAL_NAME_INDEX_NAME) == null){
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        if (session.getAttribute(FindByIndexNameSessionRepository.PRINCIPAL_NAME_INDEX_NAME).equals(userIdentifiers.id) &&
+                session.getAttribute("userToken").equals(userIdentifiers.userToken)) {
+            System.err.println("user is authenticated!");
+            User user = userService.returnUserByID(Integer.parseInt(userIdentifiers.id));
+            if(user.getIsAdmin() == 1) {
+                return new ResponseEntity<>(HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
             }
         } else {
             System.err.println("user is NOT authenticated!");
